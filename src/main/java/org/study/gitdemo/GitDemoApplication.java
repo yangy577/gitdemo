98e8e59569965585d10013cb64a50990e46e2784
yy <yangy577@163.com>
2025-05-23 09:53:39 +0800

    Init Project

---
+++ src/main/java/org/study/gitdemo/GitDemoApplication.java
@@ -0,0 +1,12 @@
+package org.study.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GitDemoApplication.class, args);
+  }
+}
---
+++ src/test/java/org/study/gitdemo/GitDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package org.study.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
