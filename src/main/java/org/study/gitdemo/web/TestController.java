d23ccb0bd785ba2bfc36d792ac6f5417485e868f
yy <yangy577@163.com>
2025-05-23 21:13:59 +0800

    cherry-pick chang01 in combined

--- src/main/java/org/study/gitdemo/web/TestController.java
+++ src/main/java/org/study/gitdemo/web/TestController.java
@@ -3,4 +3,5 @@ package org.study.gitdemo.web;
 /** . ヽ( ° ▽°)ノ̲̅] */
 public class TestController {
   public static final String WORD_TEST = "wordTest";
+  public static final String WORD_TEST3 = "wordTest3";
 }
