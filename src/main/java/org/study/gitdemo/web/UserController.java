0d8e2a5848bceddfe7e903cb2bfb6df806f86c10
yy <yangy577@163.com>
2025-05-23 10:23:09 +0800

    add TestController

---
+++ src/main/java/org/study/gitdemo/web/UserController.java
@@ -0,0 +1,12 @@
+package org.study.gitdemo.web;
+
+/** . ヽ( ° ▽°)ノ̲̅] */
+public class UserController {
+  private String getName() {
+    return "name";
+  }
+
+  private String getGrade() {
+    return "grade";
+  }
+}
